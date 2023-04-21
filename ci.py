a948a0f52975b5ce2809e845cc3aa1a976e337af
Himasha Jayawardene <himasha0421@gmail.com>
2023-04-22 00:20:43 +0530

    add i/o github action

--- ci.py
+++ ci.py
@@ -3,3 +3,4 @@ import wandb
 
 if __name__=='__main__':
     print(f'The version of wandb is : {wandb.__version__}')
+    assert wandb.__version__ == '2.01.1' , f"expected wandb 2.01.1 but got {wandb.__version__}"
